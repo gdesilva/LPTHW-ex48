9457a6fb6ece0f362128498374a4b17f7826445b
gdesilva <gusdsw@outlook.com>
2017-07-06 15:17:54 +0100

    Add files via upload

---
+++ lexicon.py
@@ -0,0 +1,18 @@
+# This scanner will take a string of raw input from a user and return a sentence
+# that's composed of a list of tuples with the (TOKEN, WORD) pairings.
+# If a word isn't part of the lexicon then it should still return the WORD but
+# set the TOKEN to an error token. These error tokens will tell users they messed up.
+
+
+# take an input (function 'scan') and break it up
+
+def lexicon():
+    pass
+
+# check if a number
+
+# check the words for type against a list of tuples (TYPE, WORD)
+
+# types: directions, verbs, stops, nouns, numbers, errors (6 types)
+
+# make a sentence from the words
